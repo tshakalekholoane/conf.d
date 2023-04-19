60123f261f54e292e47d6d3ff0ddffe63e8a8e5a
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-04-19 12:53:09 +0200

    feat: create library using go_new

--- bin/go_new.bash
+++ bin/go_new.bash
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 # Creates a new Go project.
 
-set -e
-
 PROG="$(basename "$0")"
 
+usage() {
+  printf "usage:\t%s <module> [-hl]\n" "${PROG}"
+}
+
 if [[ "$#" -eq 0 ]]; then
-  printf "usage:\t%s <module>\n" "${PROG}"
+  usage
   exit
 fi
 
 PKG="$(echo "$1" | rg --only-matching '\w+$')"
-mkdir "${PKG}" && cd "${PKG}"
-go mod init "$1"
-cat > main.go << _EOF_
+MOD="$1"
+
+module() {
+  mkdir "${PKG}" && cd "${PKG}"
+  go mod init "${MOD}"
+  go work use . 2> /dev/null
+}
+
+case "$2" in
+  "")
+    module
+    cat > main.go << _EOF_
 package main
 
 import "fmt"
@@ -22,4 +33,47 @@ func main() {
   fmt.Println("WUS GOOD?")
 }
 _EOF_
-git init
+    ;;
+  -h)
+    usage
+    exit
+    ;;
+  -l)
+    module
+    cat > "${PKG}.go" << _EOF_
+package $PKG
+
+func Add(a, b int) int { return a + b }
+_EOF_
+    cat > "${PKG}_test.go" << _EOF_
+package $PKG
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		inputA, inputB, want int
+	}{
+		{2, 2, 4},
+	}
+
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			got := Add(test.inputA, test.inputB)
+			if got != test.want {
+				t.Errorf("Add(%d, %d) = %d, want %d", test.inputA, test.inputB, got, test.want)
+			}
+		})
+	}
+}
+_EOF_
+  ;;
+  *)
+    usage
+    exit 1
+    ;;
+esac
+
+if [[ ! "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = "true" ]]; then
+  git init
+fi
