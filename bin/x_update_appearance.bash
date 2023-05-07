dc7a989dc00ab9ad3e669ee8011319d7c271a6ae
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-05-07 17:00:14 +0200

    style: switch from tabs to spaces

--- bin/x_update_appearance.bash
+++ bin/x_update_appearance.bash
@@ -4,23 +4,23 @@
 PROG="$(basename "$0")"
 
 usage() {
-	printf "usage:\t%s {dark,light}\n" "${PROG}"
+  printf "usage:\t%s {dark,light}\n" "${PROG}"
 }
 
 if [[ "$#" -eq 0 || "$#" -gt 1 ]]; then
-	usage >&2
-	exit 1
+  usage >&2
+  exit 1
 fi
 
 case "$1" in
-	dark | light)
-		alacritty_set_theme "$1"
-		fish_set_theme "$1"
-		nvim_set_background "$1"
-		;;
+  dark | light)
+    alacritty_set_theme "$1"
+    fish_set_theme "$1"
+    nvim_set_background "$1"
+    ;;
 	*)
-		printf "%s: unrecognised option: %s\n" "${PROG}" "$1" >&2
-		usage
-		exit 1
-		;;
+    printf "%s: unrecognised option: %s\n" "${PROG}" "$1" >&2
+    usage
+    exit 1
+    ;;
 esac
