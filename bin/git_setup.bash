0011f11e00d3a2830954345190bcd3dcceb87836
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-08-16 20:21:04 +0200

    feat: add Git configuration

--- bin/git_setup.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Configures Git.
-
-git config --global core.editor "$(which nvim)"
-git config --global init.defaultBranch main
-git config --global user.email "mail+git@tshaka.dev"
-git config --global user.name "Tshaka Lekholoane"
-git config --global --bool push.autoSetupRemote true
