b743e3641abf1da81b3c3ecf5b3d282b46f79ed3
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-10-11 14:42:01 +0200

    feat: update statusline

--- etc/.config/nvim/lua/statusline.lua
+++ etc/.config/nvim/lua/statusline.lua
@@ -11,34 +11,4 @@ vim.api.nvim_create_autocmd({ "BufEnter", "VimEnter", "WinEnter" }, {
   callback = current_branch,
 })
 
-function current_mode()
-  local prefix = vim.fn.mode():sub(1, 1):lower()
-  if prefix == "i" then
-    return "%2* INS %*"
-  elseif prefix == "n" then
-    return "%3* NOR %*"
-  elseif prefix == "r" then
-    return "%1* REP %*"
-  elseif prefix == "v" then
-    return "%4* VIS %*"
-  else
-    return ""
-  end
-end
-
--- Gruvbox themed palette for mode indicator. See the following for
--- details https://github.com/morhetz/gruvbox.
-local foreground = "guifg=#fbf1c7"
-local palette = {
-  "#689d6a", -- Aqua.
-  "#458588", -- Blue.
-  "#928374", -- Gray.
-  "#d65d0e", -- Orange.
-}
-for index, colour in ipairs(palette) do
-  local options = { "User" .. index, "guibg=" .. colour, foreground }
-  vim.cmd.highlight(options)
-end
-
-vim.opt.showmode = false
-vim.opt.statusline = "%{%v:lua.current_mode()%} %-F %m %-r %= %{%v:lua.current_branch()%}  %l,%c %p%%  %y"
+vim.opt.statusline = "%-F %m %-r %= %{%v:lua.current_branch()%}  %l,%c %p%%  %y"
