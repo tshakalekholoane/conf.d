f348b77bc6f9d4555db66279336eb2de96d02ee5
Tshaka Lekholoane <mail+git@tshaka.dev>
2024-03-20 11:34:44 +0200

    bin/x_snip: add null checks for output parameters
    Avoids dereferencing a nil if the output is ignored.

--- bin/x_snip.d/dequeue.go
+++ bin/x_snip.d/dequeue.go
@@ -1,5 +1,7 @@
 func dequeue[T any](a []T, v *T) []T {
-	*v = a[0]
+	if v != nil {
+		*v = a[0]
+	}
 	a[0] = *new(T)
 	a = a[1:]
 	return a
--- bin/x_snip.d/pop.go
+++ bin/x_snip.d/pop.go
@@ -1,6 +1,8 @@
 func pop[T any](a []T, v *T) []T {
 	last := len(a) - 1
-	*v = a[last]
+	if v != nil {
+		*v = a[last]
+	}
 	a[last] = *new(T)
 	a = a[:last]
 	return a
