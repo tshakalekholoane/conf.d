2ab24945b8d633d6b21be0b189627fb43a6fcfd3
Tshaka Lekholoane <mail+git@tshaka.dev>
2024-06-22 16:17:38 +0200

    feat: create x_gitignore.bash binary
    Create a binary that generates gitignore files based on
    https://github.com/github/gitignore.

---
+++ bin/x_gitignore.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Generate a gitignore file in the current directory.
+
+readonly REPOSITORY="${HOME}/conf.d/bin/x_gitignore.d"
+readonly PROGRAM="$(basename "$0")"
+
+fatalln() {
+  echo "$*" >& 2
+  exit 1
+}
+
+case "$1" in
+  "" | -h)
+    printf "usage: %s [-hl] template\n" "${PROGRAM}"
+    ;;
+  ls)
+    if [[ ! -d "${REPOSITORY}" ]]; then
+      fatalln "${PROGRAM}: repository not initialised, run ${PROGRAM}: init"
+    fi
+    ls -1 "${REPOSITORY}" | rg ".gitignore" --replace ""
+    ;;
+  init)
+    if [[ -d "${REPOSITORY}" ]]; then
+      fatalln "${PROGRAM}: repository already initialised"
+    fi
+    git clone --depth 1 "https://github.com/github/gitignore" "${REPOSITORY}"
+    ;;
+  update)
+    if [[ ! -d "${REPOSITORY}" ]]; then
+      fatalln "${PROGRAM}: repository not initialised, run ${PROGRAM} init"
+    fi
+    cd "${REPOSITORY}"
+    git pull --rebase
+    ;;
+  *)
+    if [[ ! -e "${REPOSITORY}/$1.gitignore" ]]; then
+      fatalln "${PROGRAM}: $1.gitignore not found, run ${PROGRAM} ls to list files"
+    fi
+    cp "${REPOSITORY}/$1.gitignore" ./.gitignore
+esac
