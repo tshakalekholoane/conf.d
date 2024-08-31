549d33116043f9b543064a57f8e5388a7e7ce2c3
Tshaka Lekholoane <mail+git@tshaka.dev>
2024-08-31 12:11:27 +0200

    etc/nvim: delete toggle_background

--- etc/.config/nvim/lua/keymaps.lua
+++ etc/.config/nvim/lua/keymaps.lua
@@ -1,8 +1,5 @@
-local function toggle_background()
-  vim.o.background = vim.o.background == "dark" and "light" or "dark"
 local function toggle_diagnostics()
   vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-end
 end
 
 vim.keymap.set("n", "<Leader>a", "1<C-a>")
@@ -11,6 +8,5 @@ vim.keymap.set("n", "<Leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<Leader>o", ":only<CR>")
 vim.keymap.set("n", "<Leader>r", "<Esc>:edit<CR>")
 vim.keymap.set("n", "<Leader>s", "<Esc>:r! x_snip ")
-vim.keymap.set("n", "<Leader>t", toggle_background)
 vim.keymap.set("n", "<Leader>w", "<Esc>:write<CR>")
 vim.keymap.set("n", "Q", "<Nop>")
