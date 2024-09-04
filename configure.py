57b3390aae61702978760f5535fb5720029cd859
Tshaka Lekholoane <mail+git@tshaka.dev>
2024-09-04 10:34:37 +0200

    configure: load/unload launch agents

--- configure.py
+++ configure.py
@@ -2,17 +2,23 @@
 """Create and remove symbolic links for configuration files."""
 
 import shutil
+import subprocess
 import sys
 from pathlib import Path
 
 
 USAGE = "usage: configure [--help] {install, uninstall}"
-HOME = Path.home()
+HOME  = Path.home()
+
+
+agents = [
+    HOME / "conf.d" / "opt" / "dev.tshaka.remap.plist",
+]
 
 
 def link_executables():
     target_directory = HOME / "bin"
-    target_directory.mkdir(exist_ok=True)
+    target_directory.mkdir(exist_ok = True)
     source_directory = HOME / "conf.d" / "bin"
     for source in source_directory.iterdir():
         if not source.is_dir():
@@ -52,6 +58,15 @@ def link_configuration_files():
             raise ValueError(f"{config_backup} is not a directory or symbolic link")
 
 
+def load_launch_agents():
+    if sys.platform == "darwin":
+        target_directory = HOME / "Library" / "LaunchAgents"
+        for agent in agents:
+            shutil.copy(agent, target_directory)
+            target = (target_directory / agent.stem).as_posix()
+            subprocess.run(["launchctl", "load", target], check = True)
+
+
 def unlink_executables():
     target_directory = HOME / "bin"
     if not target_directory.exists():
@@ -71,6 +86,16 @@ def unlink_configuration_files():
         target = target_directory / source.stem
         if target.exists():
             target.unlink()
+
+
+def unload_launch_agents():
+    if sys.platform == "darwin":
+        target_directory = HOME / "Library" / "LaunchAgents"
+        for agent in agents:
+            if agent.exists():
+                target = target_directory / agent.stem
+                subprocess.run(["launchctl", "unload", target.as_posix()], check = True)
+                target.unlink()
 
 
 def main():
