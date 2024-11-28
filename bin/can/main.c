b60ed09f4495c5d991aa267b2c48204ad4db5e1c
Tshaka Lekholoane <mail+git@tshaka.dev>
2024-11-28 10:42:50 +0200

    bin/can: C23

--- bin/can/main.c
+++ bin/can/main.c
@@ -17,43 +17,45 @@
 static const char* usage = "usage: can [-h | -V] [--] file ...";
 
 // Objective-C messaging primitives require that functions be cast to an
-// appropriate function pointer type before being called.
+// appropriate function pointer type before being called [1].
+//
+// [1]: https://github.com/apple-oss-distributions/objc4/blob/89543e2c0f67d38ca5211cea33f42c51500287d5/runtime/message.h#L52-L53
 
 static struct objc_object* file_manager_default_manager(void) {
-  struct objc_class* file_manager = objc_getClass("NSFileManager");
+  auto file_manager = objc_getClass("NSFileManager");
   typedef struct objc_object* (*send_type)(struct objc_class*, struct objc_selector*);
-  send_type func = (send_type)objc_msgSend;
+  auto func = (send_type)objc_msgSend;
   return func(file_manager, sel_registerName("defaultManager"));
 }
 
 static struct objc_object* file_manager_string_with_file_system_representation(struct objc_object* self, const char string[static 1]) {
   typedef struct objc_object* (*send_type)(struct objc_object*, struct objc_selector*, const char*, unsigned long);
-  send_type func = (send_type)objc_msgSend;
+  auto func = (send_type)objc_msgSend;
   return func(self, sel_registerName("stringWithFileSystemRepresentation:length:"), string, strlen(string));
 }
 
 static bool file_manager_trash_item_at_url(struct objc_object* self, struct objc_object* url, struct objc_object** error) {
   typedef bool (*send_type)(struct objc_object*, struct objc_selector*, struct objc_object*, struct objc_object*, struct objc_object**);
-  send_type func = (send_type)objc_msgSend;
-  return func(self, sel_registerName("trashItemAtURL:resultingItemURL:error:"), url, NULL, error);
+  auto func = (send_type)objc_msgSend;
+  return func(self, sel_registerName("trashItemAtURL:resultingItemURL:error:"), url, nullptr, error);
 }
 
 static struct objc_object* error_localized_description(struct objc_object* self) {
   typedef struct objc_object* (*send_type)(struct objc_object*, struct objc_selector*);
-  send_type func = (send_type)objc_msgSend;
+  auto func = (send_type)objc_msgSend;
   return func(self, sel_registerName("localizedDescription"));
 }
 
 static const char* string_utf8_string(struct objc_object* self) {
   typedef const char* (*send_type)(struct objc_object*, struct objc_selector*);
-  send_type func = (send_type)objc_msgSend;
+  auto func = (send_type)objc_msgSend;
   return func(self, sel_registerName("UTF8String"));
 }
 
 static struct objc_object* url_file_url_with_path(struct objc_object* string) {
-  struct objc_class* url = objc_getClass("NSURL");
+  auto url = objc_getClass("NSURL");
   typedef struct objc_object* (*send_type)(struct objc_class*, struct objc_selector*, struct objc_object*);
-  send_type func = (send_type)objc_msgSend;
+  auto func = (send_type)objc_msgSend;
   return func(url, sel_registerName("fileURLWithPath:"), string);
 }
 
@@ -80,7 +82,7 @@ int main(int argc, char* argv[argc + 1]) {
   argv++;
 
   for (ssize_t i = 0; i < (ssize_t)argc; i++) {
-    const char* name = argv[i];
+    auto name = argv[i];
     if (__builtin_expect(strcmp(name, ".") == 0 || strcmp(name, "..") == 0 || strcmp(name, "/") == 0, false)) {
       fprintf(stderr, "\"/\", \".\", and \"..\" may not be removed.\n");
       return EXIT_FAILURE;
@@ -88,9 +90,9 @@ int main(int argc, char* argv[argc + 1]) {
   }
 
   // Avoid using the root user's trash when invoked with sudo.
-  const char* superuser = getenv("SUDO_USER");
-  if (__builtin_expect(superuser != NULL, false)) {
-    struct passwd* entry = getpwnam(superuser);
+  auto superuser = getenv("SUDO_USER");
+  if (__builtin_expect(superuser != nullptr, false)) {
+    auto entry = getpwnam(superuser);
     if (__builtin_expect(!entry || seteuid(entry->pw_uid), false)) {
       fprintf(stderr, "%s\n", strerror(errno));
       return EXIT_FAILURE;
@@ -103,15 +105,15 @@ int main(int argc, char* argv[argc + 1]) {
     argv++;
   }
 
-  int exit_code = EXIT_SUCCESS;
-  struct objc_object* file_manager = file_manager_default_manager();
+  auto exit_code = EXIT_SUCCESS;
+  auto file_manager = file_manager_default_manager();
   for (ssize_t i = 0; i < (ssize_t)argc; i++) {
-    const char* name = argv[i];
-    struct objc_object* path = file_manager_string_with_file_system_representation(file_manager, name);
-    struct objc_object* url = url_file_url_with_path(path);
+    auto name = argv[i];
+    auto path = file_manager_string_with_file_system_representation(file_manager, name);
+    auto url = url_file_url_with_path(path);
     struct objc_object* error;
     if (__builtin_expect(!file_manager_trash_item_at_url(file_manager, url, &error), false)) {
-      struct objc_object* description = error_localized_description(error);
+      auto description = error_localized_description(error);
       fprintf(stderr, "%s\n", string_utf8_string(description));
       exit_code = EXIT_FAILURE;
     }
