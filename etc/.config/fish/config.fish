52646285104bba71a87f7789f33b191af892933a
Tshaka Lekholoane <mail+git@tshaka.dev>
2025-03-01 11:51:44 +0200

    etc/fish: delete cursor settings
    4.0.0 has default settings for these.

--- etc/.config/fish/config.fish
+++ etc/.config/fish/config.fish
@@ -30,9 +30,6 @@ end
 fish_add_path --global ~/.cargo/bin ~/bin ~/go/bin
 
 fish_hybrid_key_bindings
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
 
 gpgconf --launch gpg-agent
 umask 0002
